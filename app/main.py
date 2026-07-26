fc10a5f4d840bda6f68d4de224827b9092e246d7
Ignacio <ignaciocv2000@gmail.com>
2026-07-26 18:59:41 -0400

    Agregar webhook de Meta

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI(
     title="Asistente IA de Eventos",
     version="1.0.0"
 )
-
 
 @app.get("/")
 def inicio():
@@ -12,9 +11,27 @@ def inicio():
         "mensaje": "Servidor funcionando correctamente"
     }
 
-
 @app.get("/salud")
 def salud():
     return {
         "estado": "OK"
     }
+
+
+# ---------------------------
+# WEBHOOK DE META
+# ---------------------------
+
+VERIFY_TOKEN = "mi_token_secreto"
+
+@app.get("/webhook")
+async def verificar_webhook(request: Request):
+
+    mode = request.query_params.get("hub.mode")
+    token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return int(challenge)
+
+    return {"error": "Token incorrecto"}
